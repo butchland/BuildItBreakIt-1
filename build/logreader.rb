aa47406d187e142d14d278ee6e2948ccd302c2a8
SyedAJafri <Developer.Syed.Jafri@gmail.com>
2014-09-08 18:24:03 -0400

    testing crypto file reader

--- build/logreader.rb
+++ build/logreader.rb
@@ -14,3 +14,37 @@ class LogReader
 	end
 
 end
+
+
+def main
+	log = ""
+	i = 0
+	while ARGV[i]
+		if ARGV[i] == "-K"
+			tokenre = /[a-z]*/ # temp token regex
+			token = ARGV[i + 1]
+			#TODO verification
+			i = i + 1
+		end
+		if ARGV[i + 1] == nil
+			logname = ARGV[i]
+		end
+		i = i + 1
+	end
+	if File.exists? logname
+		log = File.open(logname)
+		#puts "Success"
+	else
+		abort "invalid"
+	end
+
+	puts token
+
+	log.each_line { |line|
+		puts line
+		LogReader.decryptor(line, token) #TODO remove
+	}
+	
+end
+
+main
